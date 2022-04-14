6ded0212ccad1bd97380b9d87171482c936fdb52
Christoph Kohnen <christoph.kohnen1@gmail.com>
2022-04-14 15:42:41 +0200

    Content: add tab for adding functions & co

--- src/main/java/com/github/chaosmelone9/datavisualizer/ui/components/contentpane/ContentPane.java
+++ src/main/java/com/github/chaosmelone9/datavisualizer/ui/components/contentpane/ContentPane.java
@@ -1,30 +1,29 @@
 package com.github.chaosmelone9.datavisualizer.ui.components.contentpane;
 
 import com.github.chaosmelone9.datavisualizer.ui.components.contentpane.graph.Graph;
-import com.github.chaosmelone9.datavisualizer.ui.components.contentpane.graph.GraphFunction;
 import com.github.chaosmelone9.datavisualizer.ui.windows.MainWindow;
 
-import javax.imageio.ImageIO;
 import javax.swing.*;
-import java.awt.image.BufferedImage;
-import java.io.IOException;
 
 public class ContentPane extends JTabbedPane{
-    private final Graph graph;
-    public ContentPane(MainWindow window) throws IOException {
-        this.graph = new Graph();
-        Table table = new Table();
+    private final Graph graph = new Graph();
+    private final Table table = new Table();
+    private final GraphComponents graphComponents = new GraphComponents();
+    public ContentPane(MainWindow window) {
         add(graph, "Graph");
-        add(new JScrollPane(table), "Table");
-
-        //graph.setBackgroundColour(new Color(158, 19, 192));
-        graph.setTitle("Test title");
-        graph.addFunction(new GraphFunction());
-        BufferedImage image = ImageIO.read(window.getInstance().getFetcher().fetch("icon.png"));
-        graph.setBackgroundImage(image);
+        add(table, "Table");
+        add(graphComponents, "Functions and other markings");
     }
 
     public Graph getGraph() {
         return graph;
+    }
+
+    public Table getTable() {
+        return table;
+    }
+
+    public GraphComponents getGraphComponents() {
+        return graphComponents;
     }
 }
---
+++ src/main/java/com/github/chaosmelone9/datavisualizer/ui/components/contentpane/GraphComponents.java
@@ -0,0 +1,11 @@
+package com.github.chaosmelone9.datavisualizer.ui.components.contentpane;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class GraphComponents extends JPanel {
+    public GraphComponents() {
+        super();
+        setBackground(Color.BLUE);
+    }
+}
