bc3ef98d2aadc7f05972775ea7a577b9dcc348be
Christoph Kohnen <christoph.kohnen@gymbane.eu>
2022-09-09 10:58:57 +0200

    GraphZoomWindow: Implement GraphZoomWindow

--- src/main/java/com/github/chaosmelone9/datavisualizer/ui/components/menubar/ViewMenu.java
+++ src/main/java/com/github/chaosmelone9/datavisualizer/ui/components/menubar/ViewMenu.java
@@ -19,6 +19,7 @@
 package com.github.chaosmelone9.datavisualizer.ui.components.menubar;
 
 import com.github.chaosmelone9.datavisualizer.ui.windows.GraphCustomizerWindow;
+import com.github.chaosmelone9.datavisualizer.ui.windows.GraphZoomWindow;
 import com.github.chaosmelone9.datavisualizer.ui.windows.MainWindow;
 
 import javax.swing.*;
@@ -32,6 +33,7 @@ public class ViewMenu extends Menu {
 
         add(new CheckBoxMenuItem("Show options menu", true, actionEvent -> window.toggleOptionPane(((JCheckBoxMenuItem) (actionEvent.getSource())).isSelected())));
         add(new MenuItem("Customize Graph...", actionEvent -> new GraphCustomizerWindow(window)));
+        add(new MenuItem("Zoom...", actionEvent -> new GraphZoomWindow(window, window.getContentPane().getGraph())));
         add(detachGraph);
     }
 
---
+++ src/main/java/com/github/chaosmelone9/datavisualizer/ui/windows/GraphZoomWindow.java
@@ -0,0 +1,89 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+/*
+ *  DataVisualizer
+ *  Copyright (C) 2022 Christoph Kohnen <christoph.kohnen@gymbane.eu>
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+package com.github.chaosmelone9.datavisualizer.ui.windows;
+
+import com.github.chaosmelone9.datavisualizer.ui.components.contentpane.graph.Graph;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class GraphZoomWindow extends PopupWindow {
+    public GraphZoomWindow(MainWindow window, Graph graph) {
+        super("Zoom", window);
+        Dimension spinnerDimension = new Dimension(60, 25);
+        JSpinner minXA = new JSpinner(new SpinnerNumberModel(graph.getMinXA(), null, null, 0.1));
+        minXA.addChangeListener(e -> graph.setMinXA((double) minXA.getValue()));
+        minXA.setPreferredSize(spinnerDimension);
+        JSpinner maxXA = new JSpinner(new SpinnerNumberModel(graph.getMaxXA(), null, null, 0.1));
+        maxXA.addChangeListener(e -> graph.setMaxXA((double) maxXA.getValue()));
+        maxXA.setPreferredSize(spinnerDimension);
+        JSpinner minXB = new JSpinner(new SpinnerNumberModel(graph.getMinXB(), null, null, 0.1));
+        minXB.addChangeListener(e -> graph.setMinXB((double) minXB.getValue()));
+        minXB.setPreferredSize(spinnerDimension);
+        JSpinner maxXB = new JSpinner(new SpinnerNumberModel(graph.getMaxXB(), null, null, 0.1));
+        maxXB.addChangeListener(e -> graph.setMaxXB((double) maxXB.getValue()));
+        maxXB.setPreferredSize(spinnerDimension);
+        JSpinner minYA = new JSpinner(new SpinnerNumberModel(graph.getMinYA(), null, null, 0.1));
+        minYA.addChangeListener(e -> graph.setMinYA((double) minYA.getValue()));
+        minYA.setPreferredSize(spinnerDimension);
+        JSpinner maxYA = new JSpinner(new SpinnerNumberModel(graph.getMaxYA(), null, null, 0.1));
+        maxYA.addChangeListener(e -> graph.setMaxYA((double) maxYA.getValue()));
+        maxYA.setPreferredSize(spinnerDimension);
+        JSpinner minYB = new JSpinner(new SpinnerNumberModel(graph.getMinYB(), null, null, 0.1));
+        minYB.addChangeListener(e -> graph.setMinYB((double) minYB.getValue()));
+        minYB.setPreferredSize(spinnerDimension);
+        JSpinner maxYB = new JSpinner(new SpinnerNumberModel(graph.getMaxYB(), null, null, 0.1));
+        maxYB.addChangeListener(e -> graph.setMaxYB((double) maxYB.getValue()));
+        maxYB.setPreferredSize(spinnerDimension);
+
+        boolean xB = graph.hasSecondXAxis();
+        boolean yB = graph.hasSecondYAxis();
+
+        minXB.setEnabled(xB);
+        maxXB.setEnabled(xB);
+        minYB.setEnabled(yB);
+        maxYB.setEnabled(yB);
+
+        add(new JLabel("min. X (1st axis)"));
+        add(new JLabel("min. X (2nd axis)"));
+        add(new JLabel("min. Y (1st axis)"));
+        add(new JLabel("min. Y (2nd axis)"));
+        constraints.gridx = 1;
+        constraints.gridy = 0;
+        add(minXA);
+        add(minXB);
+        add(minYA);
+        add(minYB);
+        constraints.gridx = 2;
+        constraints.gridy = 0;
+        add(new JLabel("max. X (1st axis)"));
+        add(new JLabel("max. X (2nd axis)"));
+        add(new JLabel("max. Y (1st axis)"));
+        add(new JLabel("max. Y (2nd axis)"));
+        constraints.gridx = 3;
+        constraints.gridy = 0;
+        add(maxXA);
+        add(maxXB);
+        add(maxYA);
+        add(maxYB);
+
+        setResizable(false);
+        this.setSize(350,200);
+    }
+}
