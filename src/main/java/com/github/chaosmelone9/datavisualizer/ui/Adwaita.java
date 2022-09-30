75d877ab5a421b82dc7be22772f799313f0f3fe8
Christoph Kohnen <christoph.kohnen@gymbane.eu>
2022-09-30 15:23:44 +0200

    Core: Append license information

--- src/main/java/com/github/chaosmelone9/datavisualizer/ui/Adwaita.java
+++ src/main/java/com/github/chaosmelone9/datavisualizer/ui/Adwaita.java
@@ -1,7 +1,26 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+/*
+ *  DataVisualizer
+ *  Copyright (C) 2022 Christoph Kohnen <christoph.kohnen@gymbane.eu>
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 package com.github.chaosmelone9.datavisualizer.ui;
 
 import java.awt.*;
 
+@SuppressWarnings("unused")
 public class Adwaita {
     public static final Color BLUE1 = new Color(153, 193, 241);
     public static final Color BLUE2 = new Color(98, 160, 234);
